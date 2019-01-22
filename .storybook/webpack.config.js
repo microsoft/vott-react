1976e351b54fba0bfe434c8a17987eba40981eaa
Elizabeth Halper <elhalper@microsoft.com>
2019-01-22 09:51:28 -0800

    cleaning up comments

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,3 @@
-// const path = require('path');
-// const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
-// module.exports = (baseConfig, env, defaultConfig) => {
-//   defaultConfig.module.rules.push({
-//     test: /\.(ts|tsx)$/,
-//     loaders: ["style-loader", "css-loader", "sass-loader", "ts-loader"]
-//   });
-//   defaultConfig.plugins.push(new TSDocgenPlugin());
-//   defaultConfig.resolve.extensions.push('.ts', '.tsx');
-//   return defaultConfig;
-// };
-
-// const path = require('path');
-
 module.exports = {
   entry: '../src/index.ts',
   module: {
@@ -31,25 +17,3 @@ module.exports = {
     extensions: [ '.tsx', '.ts', '.js', '.scss' ]
   }
 };
-
-// module.exports = {
-//     mode: "development",
-//     devtool: "inline-source-map",
-//     entry: "../src/index.ts",
-//     output: {
-//       filename: "bundle.js"
-//     },
-//     resolve: {
-//       // Add `.ts` and `.tsx` as a resolvable extension.
-//       extensions: [".ts", ".tsx", ".js", ".scss"]
-//     },
-//     module: {
-//       rules: [
-//         // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-//         {
-//             test: /\.tsx?$/,
-//             loaders: ["css-loader", "sass-loader", "ts-loader"]
-//         }
-//       ]
-//     }
-//   };
--- src/components/tagsInput/tagsInput.stories.js
+++ src/components/tagsInput/tagsInput.stories.js
@@ -6,33 +6,7 @@ import { TagsInput as tagsi } from './tagsInput';
 import { wInfo } from '../../../utils';
 import { text, boolean } from '@storybook/addon-knobs/react';
 
-// storiesOf('Components/TagsInput', module).addWithJSX(
-//   'basic TagsInput',
-//   wInfo(`
-
-//   ### Notes
-
-//   This is the tagsInput Component
-
-//   ### Usage
-//   ~~~js
-//   <Button
-//     label={'Enroll'}
-//     disabled={false}
-//     onClick={() => alert('hello there')}
-//   />
-//   ~~~`
-// )(() => (
-//     <TagsInput
-//     displayName='TagsInput Component'
-//     tags={[]}
-//     onChange={(tags) => alert()}
-//     />
-//   ))
-// );
-
 storiesOf('Components/TagsInput', module)
-  // .add('TagsInput', () => <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('TagsInput', () => (
     <tagsi
       displayName='TagsInput Component'
