c32b1da2d859f62dbf617c0615ef4a99b23a3f8c
Tanner Barlow <tanner.barlow12@gmail.com>
2019-01-24 10:29:42 -0800

    Remove internal import of css file to fix build (#33)
    * Change to import scss
    
    * move react to peer and dev dependencies
    
    * Move copy css lib to deps and add install script
    
    * remove import from tagInput
    
    * Generate css

--- src/components/tagsInput/tagsInput.tsx
+++ src/components/tagsInput/tagsInput.tsx
@@ -3,7 +3,6 @@ import { WithContext as ReactTags } from "react-tag-input";
 import { KeyCodes, randomIntInRange } from "../../common/utils";
 import { ITag } from "../../models/models";
 import { tagColors } from "../common/tagColors";
-import "./tagsInput.css";
 
 export const defaultValues = {
     tagColors,
