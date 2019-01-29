7ccbe0be314100262390a19ad7946d9a53a1547c
Wallace Breza <wallace.breza@microsoft.com>
2019-01-29 15:03:40 -0800

    Added storybook support

---
+++ stories/tagsInput.stories.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { wInfo } from "../.storybook/utils";
+
+import { TagsInput } from "../src/components/tagsInput/tagsInput";
+import { TagEditorModal } from "../src/components/tagEditorModal/tagEditorModal";
+import "../src/components/tagsInput/tagsInput.scss";
+
+const tags = [{name: "Tag 1"}];
+
+storiesOf("VoTT React", module).add(
+    "TagsInputCell",
+    wInfo(`
+  ### Notes
+
+  This is the TagsInput Component`)(() => (
+        <div>
+            <div>
+                <TagsInput
+                    tags={tags}
+                    onChange={() => { return; }}
+                />
+            </div>
+        </div>
+    )),
+);
+
+storiesOf("VoTT React", module).add(
+    "TagEditorModal",
+    wInfo(`
+  ### Notes
+
+  This is the TagEditorModal Component`)(() => (
+        <div>
+            <div>
+                <TagEditorModal
+                    onOk={() => { return; }}
+                />
+            </div>
+        </div>
+    )),
+);
